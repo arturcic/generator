8f9759e3be1014d30b39f263f5f92cdee77575bd
Artur Stolear <artur.stolear@pinkroccade.nl>
2021-11-23 11:39:40 +0100

    Initial code

---
+++ app/ExampleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using AutoNotify;
+
+namespace app
+{
+    public partial class ExampleViewModel
+    {
+        [AutoNotify]
+        private string _text = "private field text";
+
+        [AutoNotify(PropertyName = "Count")]
+        private int _amount = 5;
+    }
+}
