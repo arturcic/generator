7511cb7f40ab089f4ed4541d48c06d7e46f9e874
Artur <arturcic@gmail.com>
2021-11-27 18:20:29 +0100

    added spectre.console logging sample

---
+++ app/Commands/HelloCommand.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace app.Commands;
+
+public class HelloCommand : Command<HelloCommand.HelloSettings>
+{
+    private ILogger<HelloCommand> _logger;
+    private IAnsiConsole _console;
+
+    public HelloCommand(IAnsiConsole console, ILogger<HelloCommand> logger)
+    {
+        _console = console;
+        _logger = logger;
+        _logger.LogDebug("{0} initialized", nameof(HelloCommand));
+    }
+
+    public class HelloSettings : LogCommandSettings
+    {
+        [CommandArgument(0, "[Name]")]
+        public string Name { get; set; }
+    }
+
+
+    public override int Execute(CommandContext context, HelloSettings helloSettings)
+    {
+        _logger.LogInformation("Starting my command");
+        _console.MarkupLine($"Hello, [blue]{helloSettings.Name}[/]");
+        _logger.LogInformation("Completed my command");
+
+        return 0;
+    }
+}
--- app/Program.cs
+++ app/Program.cs
@@ -1,8 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using app;
-ExampleViewModel vm = new()
+using app.Commands;
+using app.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Spectre.Console.Cli;
+
+// to retrieve the log file name, we must first parse the command settings
+// this will require us to delay setting the file path for the file writer.
+// With serilog we can use an enricher and Serilog.Sinks.Map to dynamically
+// pull this setting.
+var serviceCollection = new ServiceCollection()
+    .AddLogging(configure =>
+        configure.AddSerilog(new LoggerConfiguration()
+            // log level will be dynamically be controlled by our log interceptor upon running
+            .MinimumLevel.ControlledBy(LogInterceptor.LogLevel)
+            // the log enricher will add a new property with the log file path from the settings
+            // that we can use to set the path dynamically
+            .Enrich.With<LoggingEnricher>()
+            // serilog.sinks.map will defer the configuration of the sink to be ondemand
+            // allowing us to look at the properties set by the enricher to set the path appropriately
+            .WriteTo.Map(LoggingEnricher.LogFilePathPropertyName,
+                (logFilePath, wt) => wt.File($"{logFilePath}"), 1)
+            .CreateLogger()
+        )
+    );
+
+var registrar = new TypeRegistrar(serviceCollection);
+var app = new CommandApp(registrar);
+
+app.Configure(config =>
 {
-    Text = "Hello, World!"
-};
-Console.WriteLine("Hello, World!");
+    config.SetInterceptor(new LogInterceptor()); // add the interceptor
+    config.AddCommand<HelloCommand>("hello");
+});
+
+return app.Run(args);
